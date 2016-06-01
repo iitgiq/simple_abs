592efe6a18324ffbd8f8acd67d0fbff275acbf10
Yao Sha <yao.sha@papaly.com>
2016-06-01 16:07:58 -0700

    find_by_id not checking experiment name

--- lib/simple_abs.rb
+++ lib/simple_abs.rb
@@ -25,12 +25,12 @@ module SimpleAbs
       test_record = AbTest.create!(experiment: name, choice: test_value, impression: impression)
       ab_test_save_cookie(ab_test_name, test_record)
     elsif !test_json[:finished]
-      test_record = test_json[:id] && AbTest.find_by_id(test_json[:id])
+      test_record = test_json[:id] && AbTest.find_by(id: test_json[:id], experiment: name)
       test_value = test_record.choice rescue test_json[:choice]
       test_record.increment!(:impression, impression) if test_record
       ab_test_save_cookie(ab_test_name, test_record) if test_record
     else # Converted
-      test_record = test_json[:id] && AbTest.find_by_id(test_json[:id])
+      test_record = test_json[:id] && AbTest.find_by(id: test_json[:id], experiment: name)
       test_value = test_record.choice rescue test_json[:choice]
     end
 
@@ -47,7 +47,7 @@ module SimpleAbs
       ab_test_name = ('ab_test_' + name).to_sym
       test_json = ab_test_read_cookie(ab_test_name)
       if test_json && !test_json[:finished]
-        test_record = test_json[:id] && AbTest.find_by_id(test_json[:id])
+        test_record = test_json[:id] && AbTest.find_by(id: test_json[:id], experiment: name)
         return unless test_record # Without a record we should probably skip.
         test_record.increment!(:conversion, conversion)
         test_record.update_attribute(:converted_at, Time.now()) unless test_record.converted_at.present?
@@ -65,7 +65,7 @@ module SimpleAbs
       ab_test_name = ('ab_test_' + name).to_sym
       test_json = ab_test_read_cookie(ab_test_name)
       if test_json && test_json[:id]
-        test_record = test_json[:id] && AbTest.find_by_id(test_json[:id])
+        test_record = test_json[:id] && AbTest.find_by(id: test_json[:id], experiment: name)
         return unless test_record # Without a record we should probably skip.
         test_record.destroy if test_record
         ab_test_save_cookie(ab_test_name, test_record, 'aborted')
@@ -76,6 +76,7 @@ module SimpleAbs
   def ab_test_save_cookie(ab_test_name, test_record, finished = nil)
     cookies.permanent[ab_test_name] = test_record.cookie_string(finished)
   end
+  # Load the data from the cookie. Simply parse the cookie as a hash
   def ab_test_read_cookie(ab_test_name)
     JSON.parse(cookies[ab_test_name]).with_indifferent_access rescue nil
   end
